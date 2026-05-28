1927114dfd349c5bf8093b002e0c8a68bd5c38f1
Nhanthichcode <nhanlx151@gmail.com>
2026-05-29 01:44:20 +0700

    update server.js

--- server.js
+++ server.js
@@ -7,7 +7,11 @@ const app = express();
 
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
-app.use(cors());
+app.use(cors({
+    origin: 'https://nhanthichcode.github.io',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true
+}));
 
 cloudinary.config({ 
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
