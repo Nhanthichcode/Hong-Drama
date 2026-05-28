bdaa884302d00fafc4af8a0160f8753c5e801093
Nhanthichcode <nhanlx151@gmail.com>
2026-05-29 01:56:53 +0700

    update server.js

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ import { v2 as cloudinary } from 'cloudinary';
 
 const app = express();
 
-app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ limit: '50mb', extended: true }));
+app.use(express.json({ limit: '100mb' }));
+app.use(express.urlencoded({ limit: '100mb', extended: true }));
 app.use(cors({
     origin: 'https://nhanthichcode.github.io',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
